6379a2521442230cc5ae16de7cd93b60cf675c2f
Hoang Duy <nhduy151001@gmail.com>
2024-05-28 21:39:58 +0700

    complete navbar and sidebar for admin site

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,97 @@
 import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import CheckOut from "./pages/checkOut/CheckOut";
 import { useState } from "react";
 import LoginPopup from "./components/loginPopup/LoginPopup";
 import Product from "./pages/product/Product";
+import Dashboard from "./pages/admin/Dashboard";
+import AdminNavbar from "./components/admin/navbar/AdminNavbar";
+import Categories from "./pages/admin/Categories";
+import Products from "./pages/admin/Products";
+import Orders from "./pages/admin/Orders";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
-      <div className="app">
-        <Navbar setShowLogin={setShowLogin} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkOut" element={<CheckOut />} />
-        </Routes>
-      </div>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <MainLayout setShowLogin={setShowLogin} />
     </>
   );
 }
+
+const MainLayout = ({ setShowLogin }) => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+  const [open, setOpen] = useState(true);
+
+  return (
+    <div className="app">
+      {isAdminRoute ? (
+        <>
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <AdminNavbar open={open} setOpen={setOpen} />
+            <Main open={open}>
+              <DrawerHeader />
+              <Routes>
+                <Route path="/admin" element={<Dashboard />} />
+                <Route path="/admin/categories" element={<Categories />} />
+                <Route path="/admin/products" element={<Products />} />
+                <Route path="/admin/orders" element={<Orders />} />
+              </Routes>
+            </Main>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Navbar setShowLogin={setShowLogin} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<CheckOut />} />
+          </Routes>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ front-end/src/components/admin/navbar/AdminNavbar.jsx
@@ -0,0 +1,309 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import { styled, alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Menu from "@mui/material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import CategoryIcon from "@mui/icons-material/Category";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import DevicesOtherTwoToneIcon from "@mui/icons-material/DevicesOtherTwoTone";
+
+const drawerWidth = 240;
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const AdminNavbar = ({ open, setOpen }) => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleListItemClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch",
+      },
+    },
+  }));
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search here…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box
+              sx={{
+                flexGrow: 0,
+                mr: 2,
+                display: "flex",
+                alignItems: "center",
+                cursor: "pointer",
+              }}
+              onClick={handleOpenUserMenu}
+            >
+              <Tooltip title="Open settings">
+                <IconButton sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Typography variant="h6" component="div" sx={{ ml: 1 }}>
+                Admin
+              </Typography>
+            </Box>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <Box sx={{ display: "flex" }}>
+            <img
+              src={assets.logo}
+              alt="Logo"
+              style={{ maxWidth: "180px", width: "100%" }}
+            />
+
+            <IconButton onClick={handleDrawerClose}>
+              <MenuOpenIcon />
+            </IconButton>
+          </Box>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <Link
+            to="/admin"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <ListItemButton
+              // selected={selectedIndex === 0}
+              onClick={() => handleListItemClick(0)}
+              sx={{
+                ...(selectedIndex === 0 && {
+                  backgroundColor: "#00FFFF",
+                  color: "#1976d2",
+                }),
+              }}
+            >
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+          </Link>
+
+          <Link
+            to="/admin/categories"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <ListItemButton
+              onClick={() => handleListItemClick(1)}
+              sx={{
+                ...(selectedIndex === 1 && {
+                  backgroundColor: "#00FFFF",
+                  color: "#1976d2",
+                }),
+              }}
+            >
+              <ListItemIcon>
+                <CategoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Categories" />
+            </ListItemButton>
+          </Link>
+
+          <Link
+            to="/admin/products"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <ListItemButton
+              onClick={() => handleListItemClick(2)}
+              sx={{
+                ...(selectedIndex === 2 && {
+                  backgroundColor: "#00FFFF",
+                  color: "#1976d2",
+                }),
+              }}
+            >
+              <ListItemIcon>
+                <DevicesOtherTwoToneIcon />
+              </ListItemIcon>
+              <ListItemText primary="Products" />
+            </ListItemButton>
+          </Link>
+
+          <Link
+            to="/admin/orders"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <ListItemButton
+              onClick={() => handleListItemClick(3)}
+              sx={{
+                ...(selectedIndex === 3 && {
+                  backgroundColor: "#00FFFF",
+                  color: "#1976d2",
+                }),
+              }}
+            >
+              <ListItemIcon>
+                <ReceiptIcon />
+              </ListItemIcon>
+              <ListItemText primary="Orders" />
+            </ListItemButton>
+          </Link>
+        </List>
+        <Divider />
+      </Drawer>
+    </Box>
+  );
+};
+
+export default AdminNavbar;
